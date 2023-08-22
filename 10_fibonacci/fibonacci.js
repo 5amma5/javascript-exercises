408a3e4c9b073598f0f97c671136efbb808d1c00
5amma5 <32971882+5amma5@users.noreply.github.com>
2023-08-22 16:30:44 -0600

    solution to 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
+  if (n ==0 ) return 0;
+  if(n == 1 || n == 2) { return 1 }
 
+  let num1 = 0, num2 = 1, nextNum;
+
+  for (let i = 1; i < n; i++) {
+    nextNum = num1 + num2;
+    num1 = num2;
+    num2 = nextNum;
+  }
+  
+  return nextNum;
 };
 
 // Do not edit below this line
