b82ae24d99f0d998181102680556bb2c9ef49db2
5amma5 <32971882+5amma5@users.noreply.github.com>
2023-08-22 14:46:06 -0600

    solution to 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((accum, currentValue) => accum + currentValue, 0);
+  // const sum = numArray.reduce((accum, currentValue) => accum + currentValue, 0);
+  // return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+	return numArray.reduce((accum, currentValue) => accum * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  if (num < 0) {return "Number must be 0 or greater"}
+  if (num === 2) { result = 2 }
+  if (num > 2) {
+    for (let count = 1; count <= num; count++) {
+      result *= count;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
