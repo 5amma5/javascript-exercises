229dd175902e5a423cefc94d31f35194c221e1ea
5amma5 <32971882+5amma5@users.noreply.github.com>
2023-08-22 15:41:43 -0600

    solution to 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanedStr = string.toLowerCase().replace(/[^a-z0-9]/g,"");
+  const reversedString = cleanedStr.split("").reverse().join("");
+  return cleanedStr === reversedString;
 };
 
 // Do not edit below this line
